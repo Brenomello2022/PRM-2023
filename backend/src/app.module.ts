44d5d03ab5fff694319c4c15a05dccd31986b2fb
Brenomello2022 <brenodemello2022@gmail.com>
2023-09-20 18:52:45 -0300

    Aula (Avaliação 19-09-2023)

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -7,6 +7,9 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
 import { UserService } from './services/user.service';
 import { UserController } from './controllers/user.controller';
+import { Topic } from './entities/topic.entity';
+import { TopicController } from './controllers/topic.controller';
+import { TopicService } from './services/topic.service';
 
 
 @Module({
@@ -18,12 +21,12 @@ import { UserController } from './controllers/user.controller';
       username: 'root',
       password: 'root',
       database: 'prm_2023',
-      entities: [User],
+      entities: [User, Topic],
       synchronize: true,
     }),
-    TypeOrmModule.forFeature([User])
+    TypeOrmModule.forFeature([User, Topic])
   ],
-  controllers: [AppController, ProfileController, UserController],
-  providers: [AppService, ProfileService, UserService],
+  controllers: [AppController, ProfileController, UserController, TopicController],
+  providers: [AppService, ProfileService, UserService, TopicService],
 })
 export class AppModule {}
